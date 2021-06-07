060807cdef498c3f8cac8c15742c210d6252fd30
Mohammad Hossein Ziyaaddini <iammhz77@gmail.com>
2021-06-07 13:29:07 +0430

    done handlers

--- behavioral/chain_of_responsibility.py
+++ behavioral/chain_of_responsibility.py
@@ -8,15 +8,21 @@ from abc import ABC
 class Handler(ABC):
 
     def __init__(self, next_handler=None):
-        self._next_handler = next_handler
+        self.__next_handler = next_handler
 
     def add_next_handler(self, next_handler):
-        self._next_handler = next_handler
+        self.__next_handler = next_handler
         return next_handler
 
     @property
     def next_handler(self):
-        return self._next_handler
+        return self.__next_handler
+
+    def check_next(self, request):
+        if self.__next_handler is not None:
+            return self.__next_handler.handle(request)
+
+        return False
 
     def handle(self, request):
         pass
@@ -24,14 +30,29 @@ class Handler(ABC):
 
 class User:
 
-    def __init__(self, username, password):
+    def __init__(self, username, password, role='simple_user'):
         self.__user_name = username
+        self.__password = password
+        self.__role = role
+
+    @property
+    def user_name(self):
+        return self.__user_name
+
+    @property
+    def role(self):
+        return self.__role
 
 
 class Request:
-    def __init__(self, user, data):
+    def __init__(self, user, data=None, request_type='GET'):
         self.__user = user
         self.__data = data
+        self.__request_type = request_type
+
+    @property
+    def request_type(self):
+        return self.__request_type
 
     @property
     def data(self):
@@ -42,13 +63,14 @@ class Request:
         return self.__user
 
 
-class AuthenticationHandler(Handler):
+class PasswordAuthenticationHandler(Handler):
 
     def __init__(self, next_handler=None):
         super().__init__(next_handler)
 
     def handle(self, request):
         print('Handling Authentication')
+        return self.check_next(request)
 
 
 class DataValidationHandler(Handler):
@@ -57,7 +79,8 @@ class DataValidationHandler(Handler):
         super().__init__(next_handler)
 
     def handle(self, request):
-        print("Handling data validation")
+        print('Handling data validation')
+        return self.check_next(request)
 
 
 class UserPermissionHandler(Handler):
@@ -67,7 +90,15 @@ class UserPermissionHandler(Handler):
 
     def handle(self, request):
         print('Handling Permissions')
+        return self.check_next(request)
 
 
 if __name__ == '__main__':
-    pass
+    user = User('iam_mhz', '12345')
+
+    password_authentication = PasswordAuthenticationHandler()
+    password_authentication. \
+        add_next_handler(UserPermissionHandler()) \
+        .add_next_handler(DataValidationHandler())
+
+    password_authentication.handle(request=Request(user))
